f8a69cd51fa4d8e0ed618286daa652a9eb03fbba
Crawford Leeds <crawford@cocoa.me>
2019-03-17 15:43:23 -0600

    start change of summary component to include days/hours instead of nights

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -5,6 +5,11 @@
         <span class="trvl-picker-summary-nights">
             {{ nights }} {{ nights === 1 ? 'night' : 'nights' }}
         </span>
+        <span class="trvl-picker-summary-duration">
+          {{ duration.days > 0 ? (duration.days + (duration.days === 1 ? ' day' : ' days') ) : ''}}
+          {{ duration.days > 0 && duration.hours > 0 ? ', ' : '' }}
+          {{ duration.hours > 0 ? (duration.hours + (duration.hours === 1 ? 'hour': 'hours') ) : ''}}
+        </span>
     </p>
 </template>
 
@@ -22,6 +27,14 @@
             },
         },
         computed: {
+            duration(){
+              const msToDays = 1000 * 60 * 60 * 24 // 1000 ms in a sec, 60 sec in a min, 60 min in an hour, 24 hours in a day
+              const msToHours = 1000 * 60 * 60 // 1000 ms in a sec, 60 sec in a min, 60 min in an hour
+              // return {days: days, hours: hours}
+              var days = Math.floor((this.checkout - this.checkin) / msToDays)
+              var hours = Math.round(((this.checkout - this.checkin) - (days * msToDays)) / msToHours * 4) / 4 // round to nearest 0.25 hour.
+              return {days: days, hours: hours}
+            },
             nights() {
                 return Math.ceil((this.checkout - this.checkin) / (1000 * 60 * 60 * 24))
             },
@@ -49,6 +62,12 @@
             color: $gray;
             font-size: 13px;
             font-weight: normal;
+        }
+        &-duration {
+          margin-left: 8px;
+          color: $gray;
+          font-size: 13px;
+          font-weight: normal;
         }
     }
 </style>
